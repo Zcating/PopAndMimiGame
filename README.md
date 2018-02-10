e93f4536842beffe04be6668d191303ead884afc
Zcating <zcating@Qq.com>
2018-02-10 13:43:25 +0800

    add content.

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Pop & Mimi Game
 This is a game about Pop Team Epic. 
+
+![example](./images/example.gif)
---
+++ project/PopAndMimiGame/Classes/AppDelegate.m
@@ -0,0 +1,45 @@
+//
+//  AppDelegate.m
+//  PopTeamEpicGame
+//
+//  Created by  zcating on 2018/2/8.
+//  Copyright © 2018 com.zcating. All rights reserved.
+//
+
+#import "AppDelegate.h"
+
+@interface AppDelegate ()
+
+@end
+
+@implementation AppDelegate
+
+
+- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
+    
+    
+    return YES;
+}
+
+
+- (void)applicationWillResignActive:(UIApplication *)application {
+}
+
+
+- (void)applicationDidEnterBackground:(UIApplication *)application {
+}
+
+
+- (void)applicationWillEnterForeground:(UIApplication *)application {
+}
+
+
+- (void)applicationDidBecomeActive:(UIApplication *)application {
+}
+
+
+- (void)applicationWillTerminate:(UIApplication *)application {
+}
+
+
+@end
